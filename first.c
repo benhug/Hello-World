11e4b248b2e8c20b6102e3e6ce5ec6bfbd93f77a
Benutzer <justynapawlowska@beneks-imac-10.fritz.box>
2013-05-26 21:57:36 +0200

    Inital Version

---
+++ first.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  first.c
+ *
+ *    Description:  My first file.
+ *
+ *        Version:  1.0
+ *        Created:  26.05.2013 18:58:31
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Moritz Broghammer (), MBroghammer@web.de
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+   printf("Hello World!");
+   return EXIT_SUCCESS;
+}			       
+
+
